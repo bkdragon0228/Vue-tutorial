708d30f6f5e198eaecc61df71da32cb6241a5221
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-08 22:31:25 +0900

    생성 : 컴포넌트 하나 생성
    컴포넌트 하나 생성하고 데이터 하나 만들었다.
    해당 컴포넌트에만 적용되는 css를 만들었다.

---
+++ project/src/views/1_databinding/DataBindingStringView.vue
@@ -0,0 +1,18 @@
+<template>
+    
+</template>
+
+<script>
+export default {
+    name : 'DataBinding',
+    setup() {
+        return {
+            userName : 'bkboy'
+        }
+    }
+}
+</script>
+
+<style scoped>
+/* 컴포넌트에 한정되는 css  */
+</style>
