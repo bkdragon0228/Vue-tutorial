3b5c7d4d2c6c7f79770dd14547ff73f9afb5458d
bkdragon0228 <rlaqjarb2067@naver.com>
2022-07-09 12:12:43 +0900

    추가 : 숫자 실시간 더하기

--- project/src/views/1_databinding/DataBindingInput.vue
+++ project/src/views/1_databinding/DataBindingInput.vue
@@ -2,6 +2,10 @@
     <div>
         <input type="text" v-model="userId"/>
         <button @click="myFunction">클릭</button>
+        <br/>
+        <input type="text" v-model.number="num1" /> +
+        <input type="text" v-model.number="num2" /> =
+        <span>{{num1 + num2}}</span>
     </div>
 </template>
 
@@ -9,9 +13,16 @@
 
 export default {
     name: 'DataBindingInput',
+    data() {
+        return {
+            num1: 0,
+            num2: 0
+        }
+    },
     setup() {
         return {
             userId: '하하'
+
         }
     },
     methods: {
